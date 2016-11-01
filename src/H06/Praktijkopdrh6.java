f926efba814f0c4110da3fe5bad98f9dcbbc9922
okan <2045338@talnet.nl>
2016-11-01 10:17:28 +0100

    praktijkopdracht6

--- src/H06/Praktijkopdrh6.java
+++ src/H06/Praktijkopdrh6.java
@@ -5,38 +5,35 @@ import java.awt.*;
 
 
 public class Praktijkopdrh6 extends Applet {
-    double cijfer1;
-    double cijfer2;
-    double cijfer3;
+    private double cijfer1;
+    private double cijfer2;
+    private double cijfer3;
 
 
-    double uitkomst;
     double gemiddelde;
     int afgekapt;
-
+    double uitkomst;
 
     public void init() {
         super.init();
         cijfer1 = 5.9;
         cijfer2 = 6.3;
         cijfer3 = 6.9;
+
         gemiddelde = (cijfer1 + cijfer2 + cijfer3) / 3;
+        afgekapt = (int) (gemiddelde * 10);
+        uitkomst = (double) afgekapt / 10;
 
     }
 
-        public void paint(Graphics g){
-            super.paint(g);
-            g.drawString(" "+ gemiddelde,50,50);
-
-
-
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawString("de uitkomst is" + uitkomst, 50, 50);
 
 
     }
 
 
-
-
-    }
+}
 
 
